bf4f3eee3fad49faf85e94f68503f11647027608
Denys Saienko <denys.saienko@droid-technologies.com>
2025-09-13 02:15:02 +0300

    Updated Hiking app

---
+++ Apps/Hiking/Software/Apps/TouchGFX-GUI/gui/include/gui/model/Model.hpp
@@ -0,0 +1,157 @@
+#ifndef MODEL_HPP
+#define MODEL_HPP
+
+#include <vector>
+#include <memory>
+
+
+#include "SDK/GSModel/GSModelHelper.hpp"
+
+#include "touchgfx/UIEventListener.hpp"
+#include "gui/common/GuiConfig.hpp"
+
+#include "Settings.hpp"
+#include "ActivitySummary.hpp"
+#include "TrackInfo.hpp"
+
+
+
+class FrontendApplication;
+class ModelListener;
+
+class Model : public touchgfx::UIEventListener,
+    public SDK::Interface::IUserApp::Callback,
+    public IGUIModelHandler
+{
+public:
+    Model();
+
+    void bind(ModelListener *listener)
+    {
+        modelListener = listener;
+    }
+
+    // Controls
+    FrontendApplication &application();
+    void tick();
+    void handleKeyEvent(uint8_t c);
+    void invalidate();
+
+    /**
+     * @brief Resets the idle timer.
+     * This method is called when a user interaction occurs, such as a key press.
+     * It prevents the application from going idle for a specified timeout period.
+     */
+    void resetIdleTimer();
+
+    /**
+     * @brief Exits the application.
+     * This method notifies the kernel that the application is exiting and performs
+     * any necessary cleanup before termination.
+     */
+    void exitApp();
+
+    // Menu position
+    void setMenuPosEnterMenu(uint16_t p);
+    uint16_t getMenuPosEnterMenu();
+    void setMenuPosTrack(uint16_t p);
+    uint16_t getMenuPosTrack();
+    void setMenuPosTrackAction(uint16_t p);
+    uint16_t getMenuPosTrackAction();
+    void setMenuPosMenuSettings(uint16_t p);
+    uint16_t getMenuPosMenuSettings();
+    void setMenuPosMenuAlerts(uint16_t p);
+    uint16_t getMenuPosMenuAlerts();
+
+    // Date/Time
+    void getDate(uint8_t& m, uint8_t& d, uint8_t& wd);
+    void getTime(uint8_t& h, uint8_t& m, uint8_t& s);
+    std::tm getDateTime();
+
+    // Power
+    uint8_t getBatteryLevel();
+
+    // Settings
+    bool isUnitsImperial();
+    const Settings& getSettings() const;
+    void setSettings(const Settings& sett);
+
+    // GPS
+    bool getGpsFix();
+
+    // Track
+    void trackStart();
+    bool trackIsActive();
+    bool trackIsPaused();
+    const Track::Data& getTrackData() const;
+    void saveTrack();
+    void discardTrack();
+    bool trackIsSummaryAvailable();
+    const ActivitySummary& trackSummary();
+
+protected:
+
+    /**
+     * @brief Decrease the idle timer.
+     * This method is called periodically to decrement the idle timer.
+     * If the timer reaches zero, it triggers an idle timeout event.
+     */
+    void decIdleTimer();
+
+    // IUserApp implementation
+    virtual void onCreate()  override;
+    virtual void onStart()   override;
+    virtual void onResume()  override;
+    virtual void onFrame()   override;
+    virtual void onPause()   override;
+    virtual void onStop()    override;
+    virtual void onDestroy() override;
+
+    // IGUIModelHandler implementation
+    virtual void handleEvent(const S2GEvent::Time& event) override;
+    virtual void handleEvent(const S2GEvent::SettingsUpd& event) override;
+    virtual void handleEvent(const S2GEvent::Battery& event) override;
+    virtual void handleEvent(const S2GEvent::GpsFix& event) override;
+    virtual void handleEvent(const S2GEvent::TrackStateUpd& event) override;
+    virtual void handleEvent(const S2GEvent::TrackDataUpd& event) override;
+    virtual void handleEvent(const S2GEvent::LapEnded& event) override;
+    virtual void handleEvent(const S2GEvent::Summary& event) override;
+
+    const IKernel*             mKernel;
+    ModelListener*             modelListener;
+    std::shared_ptr<GSModelGUI> mGSModel;
+
+    // User data
+    uint32_t mIdleTimer = 0;
+    bool mInvalidate = false;
+
+    // Menu positions
+    struct {
+        uint16_t enter;
+        uint16_t track;
+        uint16_t action;
+        uint16_t settings;
+        uint16_t alerts;
+    } mMenuPosition {};
+
+    std::tm mTime {};
+
+    // Kernel settings
+    bool mUnitsImperial = false;
+    std::array<uint8_t, 4> mHrThresholds { 150, 170, 190, 210 };
+
+    // Application settings
+    Settings mSettings {};
+
+    // Kernel state
+    bool mGpsFix = false;
+    uint8_t mBatteryLevel = 0;
+
+    // Track
+    Track::State mTrackState {};
+    std::shared_ptr<const ActivitySummary> mpActivitySummary = nullptr;
+    Track::Data mTrackData {};
+
+};
+
+#endif // MODEL_HPP
---
+++ Apps/Hiking/Software/Libs/Header/Service.hpp
@@ -0,0 +1,158 @@
+#ifndef __SERVICE_HPP__
+#define __SERVICE_HPP__
+
+#include "SDK/GSModel/GSModelHelper.hpp"
+
+#include "SDK/Interfaces/ISensorDriver.hpp"
+#include "SDK/Interfaces/ISensorDataListener.hpp"
+#include "SDK/TrackMap/TrackMapBuilder.hpp"
+
+#include "SettingsSerializer.hpp"
+#include "ActivitySummarySerializer.hpp"
+#include "ActivityWriter.hpp"
+
+
+class Service : public IServiceModelHandler,
+    public SDK::Interface::IUserApp::Callback,
+                public SDK::Interface::ISensorDataListener
+{
+public:
+    Service(const IKernel& kernel);
+
+    virtual ~Service() = default;
+
+    void run();
+
+
+private:
+    virtual void onCreate()  override;
+    virtual void onStart()   override;
+    virtual void onResume()  override;
+    virtual void onStop()    override;
+    virtual void onPause()   override;
+    virtual void onDestroy() override;
+
+    virtual void onNewSensorData(const SDK::Interface::ISensorDriver* sensor,
+        const std::vector<SDK::Interface::ISensorData*>& data, bool first) override;
+
+    virtual void handleEvent(const G2SEvent::GuiRun& event) override;
+    virtual void handleEvent(const G2SEvent::GuiStop& event) override;
+    virtual void handleEvent(const G2SEvent::TrackStart& event) override;
+    virtual void handleEvent(const G2SEvent::TrackStop& event) override;
+    virtual void handleEvent(const G2SEvent::SettingsSave& event) override;
+    virtual void handleEvent(const G2SEvent::InternalRefresh& event) override;
+
+
+    // Force refresh service state
+    void refreshService();
+
+    void sendInitialInfoToGui();
+
+
+
+    const IKernel&                  mKernel;
+    std::shared_ptr<GSModelService> mGSModel;
+    bool                            mTerminate;
+    bool                            mGUIStarted;
+
+    Settings mSettings;
+    SettingsSerializer mSettingsSerializer;
+    
+    ActivitySummary mSummary;
+    ActivitySummarySerializer mActivitySummarySerializer;
+    SDK::TrackMapBuilder mTrackMapBuilder;
+    ActivityWriter mActivityWriter;
+
+    // Sensors
+    SDK::Interface::ISensorDriver* mGpsSensor = nullptr;
+    SDK::Interface::ISensorDriver* mStepCounterSensor = nullptr;
+    SDK::Interface::ISensorDriver* mFloorCounterSensor = nullptr;
+    SDK::Interface::ISensorDriver* mAltimeterSensor = nullptr;
+    SDK::Interface::ISensorDriver* mHrSensor = nullptr;
+
+    static constexpr uint32_t skInitialSamplePeriod = 1000;
+    static constexpr uint32_t skSamplePeriod = 10000;
+    static constexpr uint32_t skSampleLatency = 0;
+
+    static constexpr float skMapDistanceThreshold = 10.0f; // meters
+
+    struct {
+        // Last sensor data
+        bool  fix;          // GPS fix
+        float latitude;     // degrees
+        float longitude;    // degrees
+        float altitude;     // meters
+        uint32_t timestamp; // ms
+    } mGps {};
+
+    struct {
+        // Last sensor data
+        uint32_t steps;     // steps
+        uint32_t timestamp; // ms
+
+        // Working data
+        uint32_t initialSteps = 0; // 0 - not avaliable
+    } mStepCounter {};
+
+    struct {
+        // Last sensor data
+        uint32_t floors;     // floors
+        uint32_t timestamp; // ms
+
+        // Working data
+        uint32_t initialFloors = 0; // 0 - not avaliable
+    } mFloorsCounter {};
+
+    struct {
+        // Last sensor data
+        float    altitude;  // meters
+        uint32_t timestamp; // ms
+
+        // Working data
+        float initialAltitude = 0.0f; // 0 - not avaliable
+
+        float ascent;           // m
+        float descent;          // m
+
+        float lapAscent;        // m
+        float lapDescent;       // m
+    } mAltimeter {};
+    
+    struct {
+        // Last sensor data
+        uint8_t  hr;        // bpm
+        uint32_t timestamp; // ms
+
+        // Working data
+        float    totalSum;
+        uint32_t totalCnt;
+
+        float    lapSum;
+        uint32_t lapCnt;
+    } mHr {};
+
+    Track::State mTrackState = Track::State::INACTIVE;
+    std::time_t mTrackStartUTC = 0;
+    bool mPreviousGpsFixState = false;
+    std::time_t mTrackProcessTimestamp = 0;
+    SDK::TrackMapBuilder::GpsPoint mPrevGpsPoint {};
+
+    Track::Data mTrackData {};
+
+    void startTrack();
+    void processTrack(std::time_t utc);
+    void stopTrack(bool discard);
+
+    enum class LapDivSource {
+        OFF = 0,
+        STEPS,
+        DISTANCE,
+        TIME,
+    };
+    LapDivSource getLapDivSource();
+    uint32_t getCurrentLap();
+    static uint32_t ParseVersion(const char* str);
+
+};
+
+#endif  // __SERVICE_HPP__
