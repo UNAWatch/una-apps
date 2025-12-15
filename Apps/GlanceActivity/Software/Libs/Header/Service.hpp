36e9e5f5d6524c68b2dd67896d580f899176eaf4
Oleksandr Tymoshenko <oleksandr.tymoshenko@droid-technologies.com>
2025-12-15 18:09:44 +0200

    developed GlanceARHR, GlanceActivity and rebuilt all glances

---
+++ Apps/GlanceActivity/Software/Libs/Header/Service.hpp
@@ -0,0 +1,43 @@
+#ifndef __SERVICE_HPP__
+#define __SERVICE_HPP__
+
+#include <ctime>
+
+#include "SDK/Kernel/Kernel.hpp"
+#include "SDK/Glance/GlanceControl.hpp"
+#include "SDK/SensorLayer/SensorConnection.hpp"
+#include "SDK/Interfaces/ISensorDataListener.hpp"
+
+class Service : public SDK::Interface::ISensorDataListener
+{
+public:
+    Service(SDK::Kernel &kernel);
+
+    virtual ~Service();
+
+    void run();
+
+private:
+    void connect();
+    void disconnect();
+
+    // ISensorDataListener implementation
+    void onSdlNewData(uint16_t                 handle,
+                      const SDK::Sensor::Data* data,
+                      uint16_t                 count,
+                      uint16_t                 stride) override;
+
+    void onGlanceTick();
+    bool configGui();
+    void createGuiControls();
+
+    const SDK::Kernel&       mKernel;
+    const char*              mName;
+    SDK::Glance::Form        mGlanceUI;
+    SDK::Glance::ControlText mGlanceTitle;
+    SDK::Glance::ControlText mGlanceValue;
+
+    SDK::Sensor::Connection mSensorActivity;
+};
+
+#endif
