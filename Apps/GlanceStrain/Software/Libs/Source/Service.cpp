1b3da87c622577e786b0d1993dbc32ce9fca880a
avatarsd <avatarsd2@gmail.com>
2026-01-29 13:59:28 +0200

    feat(glancestrain): add GlanceStrain glance app
    Introduce new Glance app with CMake and CubeIDE build configurations,
    service skeleton for sensor data display (activity/strain), icons, and
    project structure including gitignores and linker script.

---
+++ Apps/GlanceStrain/Software/Libs/Source/Service.cpp
@@ -0,0 +1,171 @@
+#include "Service.hpp"
+#include "SDK/Kernel/KernelProviderService.hpp"
+
+#define LOG_MODULE_PRX      "Service"
+#define LOG_MODULE_LEVEL    LOG_LEVEL_INFO
+#include "SDK/UnaLogger/Logger.h"
+
+#include "SDK/Messages/CommandMessages.hpp"
+#include "SDK/Messages/SensorLayerMessages.hpp"
+#include "SDK/SensorLayer/SensorDataBatch.hpp"
+
+#include "SDK/SensorLayer/DataParsers/SensorDataParserActivity.hpp"
+
+#include "icon_60x60.h"
+#include "icon_30x30.h"
+
+Service::Service(SDK::Kernel &kernel)
+    : mKernel(kernel)
+    , mName("Activity Min.")
+    , mGlanceUI()
+    , mGlanceTitle()
+    , mGlanceValue()
+    , mSensorActivity(SDK::Sensor::Type::ACTIVITY)
+{
+}
+
+Service::~Service()
+{
+    disconnect();
+}
+
+void Service::run()
+{
+    LOG_INFO("Started\n");
+
+    while (true) {
+        SDK::MessageBase *msg;
+
+        // Wait for command (blocks until available)
+        if(!mKernel.comm.getMessage(msg)) {
+            continue;
+        }
+
+        switch (msg->getType()) {
+            case SDK::MessageType::EVENT_GLANCE_START:
+                LOG_INFO("GLANCE is now running\n");
+                if (configGui()) {
+                    createGuiControls();
+                    connect();
+                } else {
+                    mKernel.comm.releaseMessage(msg);
+                    mKernel.sys.exit(0); // no return
+                }
+                break;
+
+            case SDK::MessageType::COMMAND_APP_STOP:
+                LOG_INFO("Force exit from the application\n");
+            case SDK::MessageType::EVENT_GLANCE_STOP:
+                LOG_INFO("GLANCE has stopped\n");
+                disconnect();
+                // We must release message because this is the last event.
+                mKernel.comm.releaseMessage(msg);
+                return;
+                break;
+
+            case SDK::MessageType::EVENT_GLANCE_TICK:
+                onGlanceTick();
+                break;
+
+            case SDK::MessageType::EVENT_SENSOR_LAYER_DATA: {
+                auto event = static_cast<SDK::Message::Sensor::EventData*>(msg);
+                this->onSdlNewData(event->handle,
+                                   event->data,
+                                   event->count,
+                                   event->stride);
+                } break;
+
+            default:
+                break;
+        }
+
+        // Release message after processing
+        mKernel.comm.releaseMessage(msg);
+    }
+}
+
+void Service::connect()
+{
+    if (!mSensorActivity.isConnected()) {
+        LOG_DEBUG("Connect to sensors...\n");
+        mSensorActivity.connect();
+    }
+}
+
+void Service::disconnect()
+{
+    if (mSensorActivity.isConnected()) {
+        LOG_DEBUG("Disconnect from sensors...\n");
+        mSensorActivity.disconnect();
+    }
+}
+
+void Service::onSdlNewData(uint16_t                 handle,
+                           const SDK::Sensor::Data* data,
+                           uint16_t                 count,
+                           uint16_t                 stride)
+{
+    SDK::Sensor::DataBatch batch(data, count, stride);
+
+    if (mSensorActivity.matchesDriver(handle)) {
+        SDK::SensorDataParser::Activity p(batch[0]);
+        mGlanceValue.print("%u", p.getDuration());
+    }
+}
+
+void Service::onGlanceTick()
+{
+    //LOG_DEBUG("Glance tick\n");
+
+    if (mGlanceUI.isInvalid()) {
+        auto* upd = mKernel.comm.allocateMessage<SDK::Message::RequestGlanceUpdate>();
+        if (upd) {
+            upd->name = mName;
+            upd->controls = mGlanceUI.data();
+            upd->controlsNumber = static_cast<uint32_t>(mGlanceUI.size());
+
+            mKernel.comm.sendMessage(upd, 100);
+            mKernel.comm.releaseMessage(upd);
+        }
+
+        mGlanceUI.setValid();
+   }
+}
+
+bool Service::configGui()
+{
+    bool status = false;
+    // Get Glance configuration
+    auto* gc = mKernel.comm.allocateMessage<SDK::Message::RequestGlanceConfig>();
+    if (gc) {
+        if (mKernel.comm.sendMessage(gc, 100) && gc->getResult() == SDK::MessageResult::SUCCESS) {
+            if (gc->maxControls >= 3) {
+                mGlanceUI.setWidth(gc->width);
+                mGlanceUI.setHeight(gc->height);
+                status = true;
+            }
+        }
+        mKernel.comm.releaseMessage(gc);
+    }
+
+    return status;
+}
+
+void Service::createGuiControls()
+{
+    mGlanceUI.createImage().init({31, 15}, {60, 60}, ICON_60X60_ABGR2222);
+
+    mGlanceTitle = mGlanceUI.createText();
+    mGlanceTitle.pos({ 20, 0 }, { 200, 25 })
+        .font(GlanceFont_t::GLANCE_FONT_POPPINS_SEMIBOLD_20)
+        .color(GlanceColor_t::GLANCE_COLOR_TEAL)
+        .setText("Activity Minutes")
+        .alignment(GlanceAlignH_t::GLANCE_ALIGN_H_CENTER);
+
+    mGlanceValue = mGlanceUI.createText();
+    mGlanceValue.pos({ 80, 28 }, { 80, 34 })
+        .font(GlanceFont_t::GLANCE_FONT_POPPINS_SEMIBOLD_30)
+        .color(GlanceColor_t::GLANCE_COLOR_WHITE)
+        .setText("")
+        .alignment(GlanceAlignH_t::GLANCE_ALIGN_H_CENTER);
+}
